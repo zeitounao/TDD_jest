378c7b452086ce881a79876d90ba722ddbeaac10
eliezer zeitoune <eliezerhzeitoune2@gmail.com>
2023-07-03 15:27:55 -0300

    checkLastEventStatus retorna done e passa no teste

--- testes/index.test.ts
+++ testes/index.test.ts
@@ -7,36 +7,51 @@ class CheckLastEventStatus {
   //   this.loadLastEventRepository = loadLastEventRepository
   // }    
 
-  async perform(groupID: string): Promise<void> {
+  async perform(groupID: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupID)
-
+    return 'done'
   }
 }
 
 interface LoadLastEventRepository {
-  loadLastEvent: (groupID: string) => Promise<void>
+  loadLastEvent: (groupID: string) => Promise<undefined>
 }
 
 // o mock ta preocupado apenas com o input de um repositorio para a aplicação funcionar
-class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
   groupID?: string
   callsCount = 0
+  output: undefined
 
-  async loadLastEvent(groupID: string): Promise<void> {
+  async loadLastEvent(groupID: string): Promise<undefined> {
     this.groupID = groupID
     this.callsCount++
+    return this.output
   }
 }
 
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock()
-    const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository)
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    const sut = new CheckLastEventStatus(loadLastEventRepository) 
+    // por padrao, o sut veio substituir o checkLastEventStatus que é o que queremos testar   
 
-    await checkLastEventStatus.perform('any_group_id')
+    await sut.perform('any_group_id')
 
     expect(loadLastEventRepository.groupID).toBe('any_group_id')
     expect(loadLastEventRepository.callsCount).toBe(1)
+    //permite chamar apenas uma verificação/comando  await this.loadLastEventRepository.loadLastEvent(groupID)
+
+  })
+
+  it('retorna o status quando não temos mais eventos', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    loadLastEventRepository.output = undefined
+    const sut = new CheckLastEventStatus(loadLastEventRepository)
+
+    const status = await sut.perform('any_group_id')
+
+    expect(status).toBe('done')
 
   })
 })
